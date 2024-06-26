3771031511bb253f5e42551058123a0ffc7efbbf
juani <juaniboccassini@gmail.com>
2024-06-26 18:28:11 -0300

    Header arreglado, formulario cotizacion terminado

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-/*Mostrar Menu Responsivo*/ 
+// Mostrar Menu Responsivo
 const toggleBtn = document.querySelector(".toggle-btn")
 const toggleBtnIcon = document.querySelector(".toggle-btn i")
 const responsiveMenu = document.querySelector(".responsive-menu")
@@ -11,9 +11,9 @@ toggleBtn.onclick = function(){
   ? "fa-solid fa-xmark"
   : "fa-solid fa-bars"
 }
-/*Mostrar Menu Responsivo*/
+// Mostrar Menu Responsivo
 
-/*Mostrar elementos al scrollear*/
+// Mostrar elementos al scrollear
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
         console.log(entry)
@@ -27,9 +27,9 @@ const observer = new IntersectionObserver((entries) => {
 
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el));
-/*Mostrar elementos al scrollear*/
+// Mostrar elementos al scrollear
 
-/*Desplegar FAQ*/
+// Desplegar FAQ
 const faqs = document.querySelectorAll(".faq")
 
 faqs.forEach(faq => {
@@ -37,54 +37,48 @@ faqs.forEach(faq => {
         faq.classList.toggle("active")
     })
 })
-/*Desplegar FAQ*/
+// Desplegar FAQ
 
-/*Validacion formulario tarifas*/
-let btnEnviarForm = document.querySelector("#btnEnviarForm")
-btnEnviarForm.addEventListener("click", (event) => {
+// Validacion formulario tarifas
+let sendBtn = document.getElementById("sendBtn");
+
+sendBtn.addEventListener("click", (event) => {
     event.preventDefault();
-    let origen = document.querySelector("#origen").value 
-    let cporigen = document.querySelector("#cporigen").value
-    let destino = document.querySelector("#destino").value
-    let cpdestino = document.querySelector("#cpdestino").value
-    let cantbultos = document.querySelector("#cantbultos").value
-    let kg = document.querySelector("#kg").value
-    // Verificar radio buttons
-    let pago1 = document.querySelector("#pago1").checked;
-    let pago2 = document.querySelector("#pago2").checked;
-    /* select */
-    let metodoDePago = document.querySelector("#metodoDePago").value;
+    const validateOrg = document.getElementById("validateOrg").value;
+    const validateDest = document.getElementById("validateDest").value;
+    const payment = document.getElementById("payment").checked;
+    const payMethod = document.getElementById("payMethod").value;
 
-    if(origen == "" || cporigen == "" || destino == "" || cpdestino == "" || cantbultos == "" || kg == "" || (!pago1 && !pago2) || metodoDePago === ""){
+    if (validateOrg === "" || validateDest === "" || payMethod === "" || !payment) {
         Swal.fire({
             icon: 'error',
             html: `
-            <h2 class= "tituloSweet">¡Hubo un error!</h2>
+            <h2 class="tituloSweet">¡Hubo un error!</h2>
             <br>
-            <p class= "pFormulario">Debes completar todos los campos</p>`,
+            <p class="pFormulario">Debes completar todos los campos</p>`,
             color: '#1b0366',
             timer: 5000,
             timerProgressBar: true,
-            showConfirmButton:false,
+            showConfirmButton: false,
             customClass: {
                 popup: 'tamañoAlertSweet',
             }
-        })
-    } else{
+        });
+    } else {
         Swal.fire({
             icon: 'success',
             html: `
-            <h2 class= "tituloSweet">¡Completado!</h2>
+            <h2 class="tituloSweet">¡Completado!</h2>
             <br>
-            <p class= "pFormulario">Gracias por contactarse cn nosotros en breve le responderemos</p>`,
+            <p class="pFormulario">Gracias por contactarse con nosotros, en breve le responderemos</p>`,
             color: '#1b0366',
             timer: 5000,
             timerProgressBar: true,
-            showConfirmButton:false,
+            showConfirmButton: false,
             customClass: {
                 popup: 'tamañoAlertSweet',
             }
-        })
+        });
     }
-})
-/*Validacion formulario tarifas*/
+});
+// Validacion formulario tarifas
